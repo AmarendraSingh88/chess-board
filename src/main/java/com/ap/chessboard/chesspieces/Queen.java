ea07585295797efc18d8d80d69d64420c85b4061
Amarendra Singh <amarendra.singh@digitalcues.com>
2020-06-26 00:16:27 +0530

    Changed logic for Queen's possible moves as it will be equivalent to all the moves of Bishop and Rook

--- src/main/java/com/ap/chessboard/chesspieces/Queen.java
+++ src/main/java/com/ap/chessboard/chesspieces/Queen.java
@@ -11,7 +11,8 @@ public class Queen implements Piece {
 
     @Override
     public List<String> getPossibleMovements(ChessBoard chessBoard, int x, int y) {
-        // Possible move shift for Queen (Can move across the board in all 8 directions)
+        //Approach-1- with same approach as for other piece types
+        /*// Possible move shift for Queen (Can move across the board in all 8 directions)
         int[] possibleShiftX = {-1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7,
                 -1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int[] possibleShiftY = {-1, -2, -3, -4, -5, -6, -7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7,
@@ -29,6 +30,13 @@ public class Queen implements Piece {
                 possibleMoves.add(chessBoard.getValueAtLocation(xP,yP));
             }
         }
+        return possibleMoves;
+    }*/
+
+        //Approach-2 as Queen's possible moves will be equivalent to all the moves of Bishop and Rook
+        List<String> possibleMoves = new ArrayList<>();
+        possibleMoves.addAll(new Bishop().getPossibleMovements(chessBoard, x, y));
+        possibleMoves.addAll(new Rook().getPossibleMovements(chessBoard, x, y));
         return possibleMoves;
     }
 }
